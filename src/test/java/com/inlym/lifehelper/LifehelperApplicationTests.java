72d59f681b74a236307b7e61a93a994099503bd6
inlym <inlym@qq.com>
2021-10-12 20:38:48 +0800

    :tada: Spring Boot 项目初始化

---
+++ src/test/java/com/inlym/lifehelper/LifehelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inlym.lifehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifehelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
