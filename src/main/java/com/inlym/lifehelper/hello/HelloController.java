f7172fd739bf85d4a4296a61cd5ece648af5a0a3
inlym <inlym@qq.com>
2021-10-13 22:06:33 +0800

    :sparkles: 新增 `HelloController` 控制器，用于探测项目连通性

---
+++ src/main/java/com/inlym/lifehelper/hello/HelloController.java
@@ -0,0 +1,17 @@
+package com.inlym.lifehelper.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+  @GetMapping("/")
+  public String index() {
+    return "hello inlym";
+  }
+
+  @GetMapping("/ping")
+  public String ping() {
+    return "pong";
+  }
+}
