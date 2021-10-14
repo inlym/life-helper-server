e9a29aa9316067a31512217dc68abf69d0043fb8
inlym <inlym@qq.com>
2021-10-14 23:53:20 +0800

    :art: 代码格式化

--- src/main/java/com/inlym/lifehelper/LifehelperApplication.java
+++ src/main/java/com/inlym/lifehelper/LifehelperApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LifehelperApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LifehelperApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(LifehelperApplication.class, args);
+  }
 }
