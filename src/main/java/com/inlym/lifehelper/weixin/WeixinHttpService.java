0c1123b34fc82aa5aa5ecec8b9105b9d5eb0bcc4
inlym <inlym@qq.com>
2021-10-23 20:58:54 +0800

    :construction: 大量文件改动

--- src/main/java/com/inlym/lifehelper/weixin/WeixinHttpService.java
+++ src/main/java/com/inlym/lifehelper/weixin/WeixinHttpService.java
@@ -5,9 +5,10 @@ import com.inlym.lifehelper.weixin.model.Code2SessionResponse;
 public interface WeixinHttpService {
     /**
      * 通过 code 获取鉴权信息
-     * @see <a href="https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/login/auth.code2Session.html">auth.code2Session</a>
      *
      * @param code 小程序端通过 wx.login 获取的 code
+     *
+     * @see <a href="https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/login/auth.code2Session.html">auth.code2Session</a>
      */
     Code2SessionResponse code2Session(String code);
 }
