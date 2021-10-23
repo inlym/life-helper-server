fcd3b1c138bf10da0cfaa20e3c1747bcda9d25c8
inlym <inlym@qq.com>
2021-10-23 14:13:35 +0800

    :zap: 在启动入口上配置开启缓存管理

--- src/main/java/com/inlym/lifehelper/LifehelperApplication.java
+++ src/main/java/com/inlym/lifehelper/LifehelperApplication.java
@@ -2,10 +2,12 @@ package com.inlym.lifehelper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LifehelperApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(LifehelperApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(LifehelperApplication.class, args);
+    }
 }
