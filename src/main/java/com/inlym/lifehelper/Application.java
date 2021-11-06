eb77b1f627eed293482c8060e2552ba219cafb70
inlym <inlym@qq.com>
2021-11-06 22:40:07 +0800

    :truck: 入口文件重命名

--- src/main/java/com/inlym/lifehelper/Application.java
+++ src/main/java/com/inlym/lifehelper/Application.java
@@ -6,8 +6,8 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class LifehelperApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(LifehelperApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
