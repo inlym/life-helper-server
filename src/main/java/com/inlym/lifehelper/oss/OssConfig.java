cc005e68ecacbf8fbac8c0336371478e8d76abe0
inlym <inlym@qq.com>
2021-11-08 20:37:48 +0800

    :art: 变更语义化装饰器

--- src/main/java/com/inlym/lifehelper/oss/OssConfig.java
+++ src/main/java/com/inlym/lifehelper/oss/OssConfig.java
@@ -2,9 +2,9 @@ package com.inlym.lifehelper.oss;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "oss")
 @Data
 public class OssConfig {
--- src/main/java/com/inlym/lifehelper/weixin/WeixinConfig.java
+++ src/main/java/com/inlym/lifehelper/weixin/WeixinConfig.java
@@ -2,9 +2,9 @@ package com.inlym.lifehelper.weixin;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "weixin")
 @Data
 public class WeixinConfig {
