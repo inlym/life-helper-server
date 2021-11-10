909a809addae73ffac083a447849158f99fd3273
inlym <inlym@qq.com>
2021-11-10 23:07:08 +0800

    :speech_balloon: 增加注释说明

--- src/main/java/com/inlym/lifehelper/weixin/WeixinConfig.java
+++ src/main/java/com/inlym/lifehelper/weixin/WeixinConfig.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * 微信（小程序）配置信息，用于与微信服务器交互
+ */
 @Component
 @ConfigurationProperties(prefix = "weixin")
 @Data
