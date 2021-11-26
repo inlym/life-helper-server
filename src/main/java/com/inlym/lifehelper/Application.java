bdc410acccf81159080d93b1c18e80773521189c
inlym <inlym@qq.com>
2021-11-27 00:05:09 +0800

    :building_construction: 开启过滤器扫描

--- src/main/java/com/inlym/lifehelper/Application.java
+++ src/main/java/com/inlym/lifehelper/Application.java
@@ -2,10 +2,12 @@ package com.inlym.lifehelper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
+@EnableCaching    // 开启缓存功能
+@ServletComponentScan    // 开启过滤器扫描
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
