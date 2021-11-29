168c40a8919b689dadcdbc1e1779efe13c433bcc
inlym <inlym@qq.com>
2021-11-29 16:46:10 +0800

    :building_construction: 开启计划任务

--- src/main/java/com/inlym/lifehelper/Application.java
+++ src/main/java/com/inlym/lifehelper/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching    // 开启缓存功能
-@ServletComponentScan    // 开启过滤器扫描
+@EnableScheduling    // 开启计划任务
+@ServletComponentScan    // 开启过滤器和监听器扫描
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
