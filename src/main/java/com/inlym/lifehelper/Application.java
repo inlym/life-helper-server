3ca201a6121a765ff73fdad43b6ba68294b85964
inlym <inlym@qq.com>
2021-12-16 20:30:11 +0800

    :building_construction: 引入 `Swagger3`

--- src/main/java/com/inlym/lifehelper/Application.java
+++ src/main/java/com/inlym/lifehelper/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @EnableCaching    // 开启缓存的支持
 @EnableScheduling    // 开启计划任务的支持
 @ServletComponentScan    // 开启过滤器和监听器扫描
+@EnableOpenApi    // 开启 Swagger3
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ src/main/java/com/inlym/lifehelper/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.inlym.lifehelper.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.inlym.lifehelper"))
+            .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("「我的个人助手」项目 API 文档")
+            .version("1.0")
+            .description("「我的个人助手」项目 API 文档")
+            .build();
+    }
+}
