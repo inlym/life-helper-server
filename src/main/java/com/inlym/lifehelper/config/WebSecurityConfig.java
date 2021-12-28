7d43f176d4252c8e55c34effc5891e675bb776fc
inlym <inlym@qq.com>
2021-12-28 23:03:45 +0800

    :building_construction: 新增 `WebSecurityConfig` 配置

---
+++ src/main/java/com/inlym/lifehelper/config/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.inlym.lifehelper.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+}
