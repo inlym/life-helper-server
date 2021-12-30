f5166c8a59cf017aa77a4b1cc3c7d2701a40d19e
inlym <inlym@qq.com>
2021-12-30 20:44:49 +0800

    :sparkles: 新增角色名称接口

---
+++ src/main/java/com/inlym/lifehelper/auth/RoleName.java
@@ -0,0 +1,18 @@
+package com.inlym.lifehelper.auth;
+
+/**
+ * 角色名称
+ */
+public interface RoleName {
+    /**
+     * 普通用户
+     */
+    String USER = "ROLE_USER";
+
+    /**
+     * 开发者
+     * <p>
+     * 主要用于调试部分接口
+     */
+    String DEVELOPER = "ROLE_DEVELOPER";
+}
