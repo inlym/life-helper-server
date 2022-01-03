24b13b85c9985a92fd9affa0c09737d98855ca15
inlym <inlym@qq.com>
2022-01-03 23:54:58 +0800

    :construction: 逐步完成权限相关内容

--- src/main/java/com/inlym/lifehelper/auth/RoleName.java
+++ src/main/java/com/inlym/lifehelper/auth/RoleName.java
@@ -15,4 +15,11 @@ public interface RoleName {
      * 主要用于调试部分接口
      */
     String DEVELOPER = "ROLE_DEVELOPER";
+
+    /**
+     * 管理员
+     * <p>
+     * 用于调试部分权限较高的接口。
+     */
+    String ADMIN = "ROLE_ADMIN";
 }
