922105a1d9f6b8b0130eaef26a3c0fca61c74514
inlym <inlym@qq.com>
2022-01-15 23:16:29 +0800

    :sparkles: 新增若干和风天气响应模型

---
+++ src/main/java/com/inlym/lifehelper/external/hefeng/model/HefengGridWeatherMinutelyResponse.java
@@ -0,0 +1,38 @@
+package com.inlym.lifehelper.external.hefeng.model;
+
+import lombok.Data;
+
+/**
+ * 分钟级降水 HTTP 请求响应数据
+ *
+ * @author inlym
+ * @see <a href="https://dev.qweather.com/docs/api/grid-weather/minutely/">分钟级降水</a>
+ * @since 2022-01-15 23:00
+ **/
+@Data
+public class HefengGridWeatherMinutelyResponse {
+    /** API 状态码 */
+    private String code;
+
+    /** 当前 API 的最近更新时间 */
+    private String updateTime;
+
+    /** 分钟降水描述 */
+    private String summary;
+
+    @Data
+    public static class GridMinutelyForecast {
+        /** 预报时间 */
+        private String fxTime;
+
+        /** 10分钟累计降水量，单位毫米 */
+        private String precip;
+
+        /**
+         * 降水类型:
+         * - rain -> 雨
+         * - snow -> 雪
+         */
+        private String type;
+    }
+}
---
+++ src/main/java/com/inlym/lifehelper/external/hefeng/model/HefengIndicesResponse.java
@@ -0,0 +1,41 @@
+package com.inlym.lifehelper.external.hefeng.model;
+
+import lombok.Data;
+
+/**
+ * 天气生活指数 HTTP 请求响应数据
+ *
+ * @author inlym
+ * @see <a href="https://dev.qweather.com/docs/api/indices/">天气生活指数</a>
+ * @since 2022-01-15 23:07
+ **/
+@Data
+public class HefengIndicesResponse {
+    /** API 状态码 */
+    private String code;
+
+    /** 当前 API 的最近更新时间 */
+    private String updateTime;
+
+    private DailyIndices[] daily;
+
+    public static class DailyIndices {
+        /** 预报日期 */
+        private String date;
+
+        /** 生活指数类型ID */
+        private String type;
+
+        /** 生活指数类型的名称 */
+        private String name;
+
+        /** 生活指数预报等级 */
+        private String level;
+
+        /** 生活指数预报级别名称 */
+        private String category;
+
+        /** 生活指数预报的详细描述，可能为空 */
+        private String text;
+    }
+}
