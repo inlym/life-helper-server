eb359b5bf377db9d966fb1440c846c307fd8d7d9
inlym <inlym@qq.com>
2022-01-15 14:13:46 +0800

    :art: 优化 `UserService`，去掉 Impl 中间层

--- src/main/java/com/inlym/lifehelper/user/UserService.java
+++ src/main/java/com/inlym/lifehelper/user/UserService.java
@@ -1,10 +1,29 @@
 package com.inlym.lifehelper.user;
 
-public interface UserService {
+import com.inlym.lifehelper.user.entity.User;
+import com.inlym.lifehelper.user.mapper.UserMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserMapper userMapper;
+
+    public UserService(UserMapper userMapper) {this.userMapper = userMapper;}
+
     /**
      * 通过 openid 获取用户 ID（用户不存在时将自动创建用户）
      *
      * @param openid 微信小程序用户唯一标识
      */
-    int getUserIdByOpenid(String openid);
+    public int getUserIdByOpenid(String openid) {
+        User user = userMapper.findUserByOpenid(openid);
+        if (user != null) {
+            return user.getId();
+        } else {
+            User newUser = new User();
+            newUser.setOpenid(openid);
+            userMapper.insertWithOpenid(newUser);
+            return newUser.getId();
+        }
+    }
 }
