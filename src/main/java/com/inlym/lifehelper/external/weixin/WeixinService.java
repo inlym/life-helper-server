48dd8a6300e68c261c9a242d29e9317645544586
inlym <inlym@qq.com>
2022-01-20 01:09:06 +0800

    :sparkles: 新增封装获取微信服务端接口调用凭证方法

--- src/main/java/com/inlym/lifehelper/external/weixin/WeixinService.java
+++ src/main/java/com/inlym/lifehelper/external/weixin/WeixinService.java
@@ -1,13 +1,25 @@
 package com.inlym.lifehelper.external.weixin;
 
 import com.inlym.lifehelper.common.exception.ExternalHttpRequestException;
+import com.inlym.lifehelper.external.weixin.model.WeixinGetAccessTokenResponse;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 public class WeixinService {
+    /** 在 Redis 中存储微信服务端接口调用凭证的键名 */
+    public static final String WEIXIN_ACCESS_TOKEN_KEY = "weixin:token";
+
     private final WeixinHttpService weixinHttpService;
 
-    public WeixinService(WeixinHttpService weixinHttpService) {this.weixinHttpService = weixinHttpService;}
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public WeixinService(WeixinHttpService weixinHttpService, StringRedisTemplate stringRedisTemplate) {
+        this.weixinHttpService = weixinHttpService;
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
 
     /**
      * 通过从微信小程序中获取的 code 换取用户对应的 openid
@@ -18,5 +30,30 @@ public class WeixinService {
         return weixinHttpService
             .code2Session(code)
             .getOpenid();
+    }
+
+    /**
+     * 更新在 Redis 中的微信服务端接口调用凭证
+     */
+    private String updateAccessToken() throws ExternalHttpRequestException {
+        WeixinGetAccessTokenResponse data = weixinHttpService.getAccessToken();
+        String accessToken = data.getAccessToken();
+        int expiration = data.getExpiresIn();
+
+        stringRedisTemplate
+            .opsForValue()
+            .set(WEIXIN_ACCESS_TOKEN_KEY, accessToken, Duration.ofSeconds(expiration));
+
+        return accessToken;
+    }
+
+    /**
+     * 用于内部使用获取微信服务端接口调用凭证
+     */
+    private String getAccessTokenInternal() throws ExternalHttpRequestException {
+        String token = stringRedisTemplate
+            .opsForValue()
+            .get(WEIXIN_ACCESS_TOKEN_KEY);
+        return token != null ? token : updateAccessToken();
     }
 }
