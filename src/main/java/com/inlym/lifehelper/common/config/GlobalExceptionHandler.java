a224cf437c84664ad32a7bc8b89f5f0ac324c743
inlym <inlym@qq.com>
2022-01-24 21:34:38 +0800

    :wheelchair: 优化全部异常处理器

--- src/main/java/com/inlym/lifehelper/common/config/GlobalExceptionHandler.java
+++ src/main/java/com/inlym/lifehelper/common/config/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package com.inlym.lifehelper.common.config;
 import com.inlym.lifehelper.common.exception.ExternalHttpRequestException;
 import com.inlym.lifehelper.common.model.ExceptionResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.internal.engine.ConstraintViolationImpl;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -30,12 +32,31 @@ public class GlobalExceptionHandler {
     }
 
     /**
+     * 请求参数，必填项未传值
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ExceptionResponse handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        return new ExceptionResponse(40001, "缺少请求参数 " + e.getParameterName());
+    }
+
+    /**
      * 数据校验不通过异常
      */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ConstraintViolationException.class)
     public ExceptionResponse handleConstraintViolationException(ConstraintViolationException e) {
-        return new ExceptionResponse(40000, "缺少参数或参数格式不正确");
+        ConstraintViolationImpl cv = (ConstraintViolationImpl) e
+            .getConstraintViolations()
+            .toArray()[0];
+
+        String property = String
+            .valueOf(cv.getPropertyPath())
+            .split("\\.")[1];
+
+        String message = cv.getMessage();
+
+        return new ExceptionResponse(40002, property + " " + message);
     }
 
     /**
