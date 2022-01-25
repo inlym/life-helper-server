ea9a039e6d879de0bab875febc34b2f855e6a8c1
inlym <inlym@qq.com>
2022-01-25 21:09:13 +0800

    :sparkles: 启动异步支持

--- src/main/java/com/inlym/lifehelper/Application.java
+++ src/main/java/com/inlym/lifehelper/Application.java
@@ -4,14 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
+/**
+ * 启动类
+ *
+ * @author inlym
+ */
 @SpringBootApplication
-@EnableCaching    // 开启缓存的支持
-@EnableScheduling    // 开启计划任务的支持
-@ServletComponentScan    // 开启过滤器和监听器扫描
-@EnableOpenApi    // 开启 Swagger3
+@EnableCaching
+@EnableScheduling
+@ServletComponentScan
+@EnableOpenApi
+@EnableAsync
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
