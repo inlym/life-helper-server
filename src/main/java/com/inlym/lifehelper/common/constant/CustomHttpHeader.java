f55555d1464744691f7a5c0184c20c9cdd5c3905
inlym <inlym@qq.com>
2022-02-21 22:30:49 +0800

    :bulb: 优化代码注释

--- src/main/java/com/inlym/lifehelper/common/constant/CustomHttpHeader.java
+++ src/main/java/com/inlym/lifehelper/common/constant/CustomHttpHeader.java
@@ -4,21 +4,15 @@ package com.inlym.lifehelper.common.constant;
  * 自定义的 HTTP 请求头
  *
  * @author inlym
- * @since 2022-01-19 00:29
+ * @date 2022-01-19
  */
 public final class CustomHttpHeader {
-    /**
-     * 唯一请求 ID，用作全链路追踪 ID
-     */
+    /** 请求 ID，用作全链路追踪 ID */
     public static final String REQUEST_ID = "X-Ca-Request-Id";
 
-    /**
-     * 客户端 IP 地址，由 API 网关层加入到请求头中
-     */
+    /** 客户端 IP 地址，由 API 网关层加入到请求头中 */
     public static final String CLIENT_IP = "X-Client-Ip";
 
-    /**
-     * 存储 JWT 的请求头
-     */
+    /** JWT 字符串 */
     public static final String JWT_TOKEN = "X-Auth-Jwt";
 }
--- src/main/java/com/inlym/lifehelper/common/constant/CustomRequestAttribute.java
+++ src/main/java/com/inlym/lifehelper/common/constant/CustomRequestAttribute.java
@@ -2,26 +2,19 @@ package com.inlym.lifehelper.common.constant;
 
 /**
  * 自定义请求属性值
- * <p>
- * 在使用 `request.getAttribute` 和 `request.setAttribute` 方法时，不要直接操作字符串，
- * 应该直接当做常量存于此处。
+ *
+ * <li> 在使用 {@code request.getAttribute} 和 {@code request.setAttribute} 方法时，不要直接操作字符串，应该直接当做常量存于此处。
  *
  * @author inlym
- * @since 2022-01-19 00:29
+ * @date 2022-01-19
  */
 public final class CustomRequestAttribute {
-    /**
-     * 唯一请求 ID
-     */
+    /** 请求 ID */
     public static final String REQUEST_ID = "REQUEST_ID";
 
-    /**
-     * 用户 ID
-     */
+    /** 用户 ID */
     public static final String USER_ID = "USER_ID";
 
-    /**
-     * 客户端的 IP 地址
-     */
+    /** 客户端 IP 地址 */
     public static final String CLIENT_IP = "CLIENT_IP";
 }
