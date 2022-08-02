c69cbf8441ee80349f348c94cb4118acd9b97515
inlym <inlym@qq.com>
2022-08-03 01:52:50 +0800

    :children_crossing: 优化 `RestTemplate`

--- src/main/java/com/inlym/lifehelper/common/config/RestTemplateConfig.java
+++ src/main/java/com/inlym/lifehelper/common/config/RestTemplateConfig.java
@@ -3,7 +3,9 @@ package com.inlym.lifehelper.common.config;
 import com.inlym.lifehelper.common.http.HttpLoggingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -30,7 +32,27 @@ public class RestTemplateConfig {
         interceptors.add(new HttpLoggingInterceptor());
 
         restTemplate.setInterceptors(interceptors);
+        restTemplate
+            .getMessageConverters()
+            .add(new CustomMappingJackson2HttpMessageConverter());
 
         return restTemplate;
+    }
+
+    /**
+     * 自定义数据转换器
+     *
+     * <h2>为什么需要这个转换器？
+     * <p>微信部分服务端 API 的 `Content-Type` 为 `text/plain`，需要额外支持解析 JSON。
+     *
+     * @since 1.3.0
+     */
+    private static class CustomMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
+        public CustomMappingJackson2HttpMessageConverter() {
+            List<MediaType> mediaTypes = new ArrayList<>();
+            mediaTypes.add(MediaType.TEXT_PLAIN);
+            mediaTypes.add(MediaType.TEXT_HTML);
+            setSupportedMediaTypes(mediaTypes);
+        }
     }
 }
