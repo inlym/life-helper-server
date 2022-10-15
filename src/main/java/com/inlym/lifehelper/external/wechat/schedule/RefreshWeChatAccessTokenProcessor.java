ae966a90c8f67863ebafeae372f94c1d6ec15a1e
inlym <inlym@qq.com>
2022-10-15 22:44:46 +0800

    :wheelchair: 优化更新微信服务端接口调用凭据定时任务

--- src/main/java/com/inlym/lifehelper/external/wechat/schedule/RefreshWeChatAccessTokenProcessor.java
+++ src/main/java/com/inlym/lifehelper/external/wechat/schedule/RefreshWeChatAccessTokenProcessor.java
@@ -12,12 +12,15 @@ import org.springframework.stereotype.Component;
  * 更新微信服务端接口调用凭据定时任务
  *
  * <h2>说明
- * <p>以下信息配置在阿里云的 <a href="https://www.aliyun.com/ntms/middleware/schedulerx?userCode=lzfqdh6g">分布式任务调度平台</a> 上，
+ * <p>当前类定义了一个定时任务，任务的执行逻辑托管在了阿里云的 <a href="https://www.aliyun.com/ntms/middleware/schedulerx?userCode=lzfqdh6g">分布式任务调度平台</a> 上，
  * 此处仅用于留存备忘。
  *
  * <li> [任务名称]：更新微信服务端接口调用凭据
- * <li> [任务描述]：用于强制更新微信服务端接口调用凭据（不管原有凭据是否有效）。
+ * <li> [任务描述]：每半小时强制更新微信服务端接口调用凭据（不管原有凭据是否有效）。
  * <li> [执行时间]：每半小时一次（即每小时的 00 和 30 分）
+ *
+ * <h2>注意事项
+ * <p>在非“生产环境”下，不要开启这个定时任务，会导致微信服务端接口调用凭据的有效性冲突。
  *
  * @author <a href="https://www.inlym.com">inlym</a>
  * @date 2022/10/15
@@ -26,7 +29,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class WeChatRefreshAccessTokenProcessor extends JavaProcessor {
+public class RefreshWeChatAccessTokenProcessor extends JavaProcessor {
     private final WeChatAccessTokenService weChatAccessTokenService;
 
     @Override
