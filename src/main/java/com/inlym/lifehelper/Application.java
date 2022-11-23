06be30a588d5d8dee07ed0cd468a5b0d867813a3
inlym <inlym@qq.com>
2022-11-23 20:35:25 +0800

    :building_construction: 新增异步配置类，配置异步方法线程池

--- src/main/java/com/inlym/lifehelper/Application.java
+++ src/main/java/com/inlym/lifehelper/Application.java
@@ -4,19 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 启动类
  *
- * @author inlym
+ * @author <a href="https://www.inlym.com">inlym</a>
  */
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
 @ServletComponentScan
-@EnableAsync
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ src/main/java/com/inlym/lifehelper/common/config/SpringAsyncConfig.java
@@ -0,0 +1,34 @@
+package com.inlym.lifehelper.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 异步方法配置
+ *
+ * @author <a href="https://www.inlym.com">inlym</a>
+ * @date 2022/11/23
+ * @since 1.7.0
+ **/
+@EnableAsync
+@Configuration
+public class SpringAsyncConfig implements AsyncConfigurer {
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(50);
+        executor.setQueueCapacity(1000);
+        executor.setKeepAliveSeconds(300);
+        executor.setThreadNamePrefix("async-thread-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        
+        return executor;
+    }
+}
