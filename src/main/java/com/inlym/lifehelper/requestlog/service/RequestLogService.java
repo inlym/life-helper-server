909641e75ac19cd441a5485f9183f3fd3cbb71ad
inlym <inlym@qq.com>
2022-11-23 20:53:50 +0800

    :wheelchair: 优化请求日志方法

--- src/main/java/com/inlym/lifehelper/requestlog/service/RequestLogService.java
+++ src/main/java/com/inlym/lifehelper/requestlog/service/RequestLogService.java
@@ -7,11 +7,11 @@ import com.inlym.lifehelper.location.pojo.IpLocation;
 import com.inlym.lifehelper.requestlog.entity.RequestLog;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * 请求日志服务
@@ -39,45 +39,36 @@ public class RequestLogService {
      * @since 1.7.0
      */
     public void resolveRequest(HttpServletRequest request) {
-        int userId = request.getAttribute(CustomRequestAttribute.USER_ID) != null ? (int) request.getAttribute(CustomRequestAttribute.USER_ID) : 0;
-        String ip = (String) request.getAttribute(CustomRequestAttribute.CLIENT_IP);
-        System.out.println(ip);
-        String method = request.getMethod();
+        List<String> whiteList = List.of("/ping");
         String path = request.getRequestURI();
-        String querystring = request.getQueryString();
-        String requestId = (String) request.getAttribute(CustomRequestAttribute.REQUEST_ID);
-        String url = path + (querystring != null ? "?" + querystring : "");
 
-        log.info("{} {} {} {} {}", requestId, ip, userId, method, url);
+        if (!whiteList.contains(path)) {
+            int userId = request.getAttribute(CustomRequestAttribute.USER_ID) != null ? (int) request.getAttribute(CustomRequestAttribute.USER_ID) : 0;
+            String ip = (String) request.getAttribute(CustomRequestAttribute.CLIENT_IP);
+            String method = request.getMethod();
+            String querystring = request.getQueryString();
+            String requestId = (String) request.getAttribute(CustomRequestAttribute.REQUEST_ID);
+            String url = path + (querystring != null ? "?" + querystring : "");
 
-        IpLocation ipLocation = locationService.locateIpUpToCity(ip);
+            log.info("{} {} {} {} {}", requestId, ip, userId, method, url);
 
-        RequestLog requestLog = RequestLog
-            .builder()
-            .userId(userId)
-            .date(LocalDate.now())
-            .requestId(requestId)
-            .method(method)
-            .path(path)
-            .querystring(querystring)
-            .ip(ip)
-            .province(ipLocation.getProvince())
-            .city(ipLocation.getCity())
-            .district(ipLocation.getDistrict())
-            .build();
+            IpLocation ipLocation = locationService.locateIpUpToCity(ip);
 
-        timeseriesExecutor.insertAsync(requestLog);
-    }
+            RequestLog requestLog = RequestLog
+                .builder()
+                .userId(userId)
+                .date(LocalDate.now())
+                .requestId(requestId)
+                .method(method)
+                .path(path)
+                .querystring(querystring)
+                .ip(ip)
+                .province(ipLocation.getProvince())
+                .city(ipLocation.getCity())
+                .district(ipLocation.getDistrict())
+                .build();
 
-    /**
-     * 处理请求日志信息（异步）
-     *
-     * @param request 请求对象
-     *
-     * @since 1.7.0
-     */
-    @Async
-    public void resolveRequestAsync(HttpServletRequest request) {
-        resolveRequest(request);
+            timeseriesExecutor.insertAsync(requestLog);
+        }
     }
 }
