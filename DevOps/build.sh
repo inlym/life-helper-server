377e5e9f8b0db990c8b2f4c6e115ea0df1506b1d
inlym <inlym@qq.com>
2023-02-06 23:47:42 +0800

    :whale: 优化 CI/CD 脚本

--- DevOps/build.sh
+++ DevOps/build.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-### 构建脚本 - 在构建机上运行，最终输出 Docker 镜像推送至镜像仓库
+################################### 项目构建脚本 ###################################
+
+### 运行说明
+# 1. 以 git 标签（即版本号）名发起构建。
+
+### 运行环境
+# 1. 由“云效”执行自动化脚本，在构建机上运行。
+
+### 构建目标
+# 输出 Docker 镜像，并上传至阿里云镜像服务。
+
+### 文档地址
+# 1. 云效-环境变量 -> https://help.aliyun.com/document_detail/153688.html?userCode=lzfqdh6g
+# 2. 阿里云-容器镜像服务 -> https://www.aliyun.com/product/acr?userCode=lzfqdh6g
 
 # 要构建的 Git Commit 的标签名，例如 `1.0.0`
 echo "${CI_COMMIT_REF_NAME}"
 
 # 将代码克隆至本地
-git clone https://github.com/inlym/life-helper-server.git  --depth=1  --branch 1.4.0 /root/workspace/life-helper-server
+git clone https://github.com/inlym/life-helper-server.git  --depth=1  --branch "${CI_COMMIT_REF_NAME}" /root/workspace/life-helper-server
 
 # 进入工作目录
 cd /root/workspace/life-helper-server || exit
--- DevOps/deploy.sh
+++ DevOps/deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# 部署脚本 - 在部署机（即应用服务器）上运行
+################################### 项目部署脚本 ###################################
+
+### 运行说明
+# 1. 需要在各个应用服务器（即部署机）上都运行一遍。
+
+### 运行环境
+# 1. 由“云效”执行自动化脚本，在部署机上运行。
+
+### 文档地址
+# 1. 云效-环境变量 -> https://help.aliyun.com/document_detail/153688.html?userCode=lzfqdh6g
+# 2. 阿里云-容器镜像服务 -> https://www.aliyun.com/product/acr?userCode=lzfqdh6g
 
 # 之前构建使用的 Git Commit 的标签名，例如 `1.0.0`
 echo "${CI_COMMIT_REF_NAME}"
