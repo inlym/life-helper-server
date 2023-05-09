57f3916689a819c64783cd86c9f785d3a26185ff
inlym <inlym@qq.com>
2023-05-09 21:24:24 +0800

    :truck: 辅助类改名

--- src/main/java/com/inlym/lifehelper/common/util/HashedIdUtil.java
+++ src/main/java/com/inlym/lifehelper/common/util/HashedIdUtil.java
@@ -5,16 +5,16 @@ import org.springframework.util.DigestUtils;
 import java.nio.charset.StandardCharsets;
 
 /**
- * ID 相关辅助方法
+ * 哈希化 ID 辅助方法
  *
  * <h2>主要用途
- * <p>封装内部可直接调用的 ID 辅助方法。
+ * <p>封装内部可直接调用的哈希化 ID 辅助方法，目前用于为表格存储生成分区键。
  *
  * @author <a href="https://www.inlym.com">inlym</a>
  * @date 2023/5/6
  * @since 2.0.0
  **/
-public abstract class IdUtil {
+public abstract class HashedIdUtil {
     /**
      * 获取哈希化的字符串主键 ID
      *
@@ -25,7 +25,7 @@ public abstract class IdUtil {
      *
      * @see <a href="https://help.aliyun.com/document_detail/142533.html">表设计</a>
      */
-    public static String getHashedId(int id) {
+    public static String create(int id) {
         String sid = String.valueOf(id);
         String prefix = DigestUtils.md5DigestAsHex(sid.getBytes(StandardCharsets.UTF_8))
                                    .substring(0, 8);
@@ -43,7 +43,7 @@ public abstract class IdUtil {
      *
      * @since 1.4.0
      */
-    public static int parseHashedId(String hashedId) {
+    public static int parse(String hashedId) {
         String[] strings = hashedId.split("_");
 
         String sid = strings[1];
