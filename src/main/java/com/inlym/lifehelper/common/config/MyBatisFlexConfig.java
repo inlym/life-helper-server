05c54f780168016994ff5daae45a75d9c0ffdbd5
inlym <inlym@qq.com>
2023-11-21 19:36:29 +0800

    :wrench: 修改 MyBatis-Flex 配置文件

--- src/main/java/com/inlym/lifehelper/common/config/MyBatisFlexConfig.java
+++ src/main/java/com/inlym/lifehelper/common/config/MyBatisFlexConfig.java
@@ -1,7 +1,11 @@
 package com.inlym.lifehelper.common.config;
 
+import com.mybatisflex.core.FlexGlobalConfig;
+import com.mybatisflex.core.audit.AuditManager;
+import com.mybatisflex.core.audit.ConsoleMessageCollector;
 import com.mybatisflex.core.logicdelete.LogicDeleteProcessor;
 import com.mybatisflex.core.logicdelete.impl.DateTimeLogicDeleteProcessor;
+import com.mybatisflex.spring.boot.MyBatisFlexCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,14 +17,35 @@ import org.springframework.context.annotation.Configuration;
  * @since 2.0.3
  **/
 @Configuration
-public class MyBatisFlexConfig {
+public class MyBatisFlexConfig implements MyBatisFlexCustomizer {
     /**
      * 配置逻辑删除处理器
      *
+     * @date 2023/10/9
      * @see <a href="https://mybatis-flex.com/zh/core/logic-delete.html">逻辑删除</a>
+     * @since 2.0.3
      */
     @Bean
     public LogicDeleteProcessor logicDeleteProcessor() {
         return new DateTimeLogicDeleteProcessor();
+    }
+
+    /**
+     * MyBatis-Flex 初始化配置
+     *
+     * @param config 公共配置
+     *
+     * @date 2023/10/10
+     * @see <a href="https://mybatis-flex.com/zh/base/mybatis-flex-customizer.html">MyBatisFlexCustomizer</a>
+     * @since 2.0.3
+     */
+    @Override
+    public void customize(FlexGlobalConfig config) {
+        AuditManager.setMessageCollector(new ConsoleMessageCollector());
+        // 开启审计功能
+        AuditManager.setAuditEnable(true);
+
+        // 关闭控制台打印标志
+        config.setPrintBanner(false);
     }
 }
