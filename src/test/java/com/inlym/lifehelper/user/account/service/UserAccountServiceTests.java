aa49accd628539580515389a913288467c06690b
inlym <inlym@qq.com>
2024-05-21 01:07:35 +0800

    :white_check_mark: 写用户账户服务的单元测试

---
+++ src/test/java/com/inlym/lifehelper/user/account/service/UserAccountServiceTests.java
@@ -0,0 +1,30 @@
+package com.inlym.lifehelper.user.account.service;
+
+import com.inlym.lifehelper.user.account.entity.User;
+import com.inlym.lifehelper.user.account.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class UserAccountServiceTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private UserAccountService userAccountService;
+
+    @Test
+    void testCreateUser() {
+        long id = userAccountService.createUser();
+
+        // 新生成的用户 ID 要求大于 0
+        Assert.isTrue(id > 0, "新生成的用户 ID 要求大于 0");
+
+        // 通过 {@code id} 查找记录，应该存在
+        User user = userMapper.selectOneById(id);
+        Assert.notNull(user, "通过 id 查找记录，应该存在");
+    }
+}
