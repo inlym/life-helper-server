94bf18e356d135ed78d18e49d895aa0580631df0
inlym <inlym@qq.com>
2024-05-23 00:03:36 +0800

    :white_check_mark: 补充用户账户服务的单元测试

--- src/test/java/com/inlym/lifehelper/user/account/service/UserAccountServiceTests.java
+++ src/test/java/com/inlym/lifehelper/user/account/service/UserAccountServiceTests.java
@@ -26,7 +26,6 @@ class UserAccountServiceTests {
 
         // 新生成的用户 ID 要求大于 0
         assertThat(id).isGreaterThan(0L);
-
         // 通过 {@code id} 查找记录，应该存在
         User user = userMapper.selectOneById(id);
         assertThat(user).isNotNull();
@@ -35,5 +34,12 @@ class UserAccountServiceTests {
         // 创建时间应该是“刚刚”（设定10秒内）
         assertThat(user.getCreateTime()).isBefore(LocalDateTime.now());
         assertThat(Duration.between(user.getCreateTime(), LocalDateTime.now())).isLessThan(Duration.ofSeconds(10L));
+        // 默认昵称和头像路径都是空字符串
+        assertThat(user.getNickName()).isEmpty();
+        assertThat(user.getAvatarPath()).isEmpty();
+        // 默认登录次数为 `1`
+        assertThat(user.getLoginCounter()).isEqualTo(1L);
+        // 再建一个 id 应该比之前的大
+        assertThat(userAccountService.createUser()).isGreaterThan(id);
     }
 }
