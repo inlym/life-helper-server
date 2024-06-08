43cf34cbf8f2f94ba1f104c9cc9ecc71ae5b6751
inlym <inlym@qq.com>
2024-06-08 15:46:18 +0800

    :sparkles: 完成 `UserUploadBucketService` 方法

---
+++ src/main/java/com/inlym/lifehelper/common/base/aliyun/oss/config/OssConfig.java
@@ -0,0 +1,43 @@
+package com.inlym.lifehelper.common.base.aliyun.oss.config;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.inlym.lifehelper.common.base.aliyun.oss.service.OssEndpointService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * OSS 客户端配置
+ *
+ * @author <a href="https://www.inlym.com">inlym</a>
+ * @date 2024/6/8
+ * @since 2.3.0
+ **/
+@Configuration
+@RequiredArgsConstructor
+public class OssConfig {
+    private final CentralBucketProperties centralBucketProperties;
+
+    private final UserUploadBucketProperties userUploadBucketProperties;
+
+    private final OssEndpointService ossEndpointService;
+
+    @Bean
+    public OSS centralBucketClient() {
+        String endpoint = ossEndpointService.getEndpoint();
+        String accessKeyId = centralBucketProperties.getAccessKeyId();
+        String accessKeySecret = centralBucketProperties.getAccessKeySecret();
+
+        return new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+    }
+
+    @Bean
+    public OSS userUploadBucketClient() {
+        String endpoint = ossEndpointService.getEndpoint();
+        String accessKeyId = userUploadBucketProperties.getAccessKeyId();
+        String accessKeySecret = userUploadBucketProperties.getAccessKeySecret();
+
+        return new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+    }
+}
---
+++ src/main/java/com/inlym/lifehelper/common/base/aliyun/oss/model/GeneratePostCredentialOptions.java
@@ -0,0 +1,35 @@
+package com.inlym.lifehelper.common.base.aliyun.oss.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Duration;
+
+/**
+ * 生成直传凭据的配置项
+ *
+ * @author <a href="https://www.inlym.com">inlym</a>
+ * @date 2024/6/8
+ * @since 2.3.0
+ **/
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GeneratePostCredentialOptions {
+    /**
+     * 上传至 OSS 后的文件路径
+     *
+     * <h3>示例值
+     * <p>{@code 20240608/r7OPIjuso1rR.png}
+     */
+    private String key;
+
+    /** 限制最大上传文件大小，单位为 MD */
+    private Long sizeMB;
+
+    /** 直传凭据有效时长 */
+    private Duration duration;
+}
