9279d048b35ae075c29f038e452900d538ca51a2
inlym <inlym@qq.com>
2024-07-25 23:45:41 +0800

    fix: fixed the issue where Mapper files in multi-module packaging were not being scanned

--- life-helper-web/src/main/java/com/weutil/web/Application.java
+++ life-helper-web/src/main/java/com/weutil/web/Application.java
@@ -1,11 +1,13 @@
 package com.weutil.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"com.weutil.**"})
 @ServletComponentScan(basePackages = {"com.weutil.**"})
+@MapperScan(basePackages = {"com.weutil.**"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
